778a8df88bb059bf6671aea5f4d1f9cf22194d48
Anhardy1999 <96258085+Anhardy1999@users.noreply.github.com>
2021-12-16 22:22:52 -0500

    added ref_call

--- Base_Calls/Random_Data_Gen.py
+++ Base_Calls/Random_Data_Gen.py
@@ -31,3 +31,40 @@ def random_gen(calls = 4, rows = 1000):
   
   return df
 
+def ref_call(df):
+  ''' Makes the ref calls based on the data in the provided dataframe to compare for errors.
+            
+            This function takes the range of base columns "A" and "T"  and uses those
+            for determining when a call calculation should be made. 
+            
+            The function checks for the greatest value between the four bases. Conditions 
+            are set for each column and will add the base with the highest "color" reading 
+            to the designated column. 
+            
+            Uses the same idea as base_call except it inserts a reference column if needed.
+  '''
+
+  bases = ['A', 'C', 'G', 'T']
+        
+  A = list(df.filter(like = 'A').columns.values)
+  T = list(df.filter(like= 'T').columns.values)
+  i = 1
+  for a, t in zip(A, T):
+    ref_start = a
+    ref_end = t
+    base_range = list(df.loc[:, ref_start:ref_end].columns.values)
+    conditions = [
+      (df[base_range[0:]].idxmax(axis = 1) == base_range[0]),
+      (df[base_range[0:]].idxmax(axis = 1) == base_range[1]),
+      (df[base_range[0:]].idxmax(axis = 1) == base_range[2]),
+      (df[base_range[0:]].idxmax(axis = 1) == base_range[3]),
+      ]
+    ref_name = 'ref_' + str(i)
+    i += 1
+    data = np.select(conditions, bases)
+    if ref_name in df.columns:
+      df = df.drop(ref_name, axis = 1)
+    loc = df.columns.get_loc(a)
+    df.insert(loc, ref_name, data)
+        
+    return df
